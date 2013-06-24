55a0fe5c8404d25bd0e18e5b735610c1aac741fb
Pavel Nezhencev <whyleee@gmail.com>
2013-06-24 14:10:05 +0300

    Refactored AssemblyInfo.cs

--- EpiTemplate/Properties/AssemblyInfo.cs
+++ EpiTemplate/Properties/AssemblyInfo.cs
@@ -1,35 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// General info about the product
 [assembly: AssemblyTitle("EpiTemplate")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Common EPiServer 7 template for web projects")]
+[assembly: AssemblyCompany("Pavel Nezhencev")]
 [assembly: AssemblyProduct("EpiTemplate")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright © Pavel Nezhencev 2013")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b626357f-ad53-4126-b6f1-8ed5e4eace83")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
+// Product version (use "1.0.*" for auto-increment)
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
